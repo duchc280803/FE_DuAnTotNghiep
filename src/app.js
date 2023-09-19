650dbd2b49a624af1f5c4c9ca53b3f9af15735bc
kjuDyHatC <duchc280803@gmail.com>
2023-09-19 17:03:09 +0700

    Điều hướng trang

--- src/app.js
+++ src/app.js
@@ -1,13 +1,34 @@
-var myApp = angular.module("myModule", []);
-myApp.controler("nhanVienController", function ($http, $scope) {
-    $scope.listNhanVien = [];
-    function fetchNhanVienList() {
-      $http
-        .get("http://localhost:8080/api/nhan-vien/hien-thi")
-        .then(function (response) {
-          $scope.listNhanVien = response.data;
-        });
-    }
-    fetchNhanVienList();
-  });
-  
+var myApp = angular.module("myModule", ["ngRoute"]);
+myApp.config(function ($routeProvider, $locationProvider) {
+  $locationProvider.hashPrefix("");
+
+  $routeProvider
+    .when("/admin/dashboard", {
+      templateUrl: "./pages/dashboard.html",
+    })
+    .when("/admin/orders", {
+      templateUrl: "./pages/bantaiquay.html",
+    })
+    .when("/admin/hoa-don", {
+      templateUrl: "./pages/thuchi.html",
+    })
+    .when("/admin/discounts", {
+      templateUrl: "./pages/khuyen-mai.html",
+    })
+    .when("/admin/discount/new", {
+      templateUrl: "./pages/create-ma-khuyen-mai.html",
+    })
+    .when("/admin/discount/new_automatic", {
+      templateUrl: "./pages/create-chuong-trinhkm.html",
+    })
+    .when("/admin/account/nhan-vien", {
+      templateUrl: "./pages/nhan-vien.html",
+      controller: "nhanVienController",
+    })
+    .when("/admin/account/khach-hang", {
+      templateUrl: "./pages/khach-hang.html",
+    })
+    .otherwise({
+      redirectTo: "/admin/dashboard",
+    });
+});
---
+++ src/controller/nhan-vien-controller.js
@@ -0,0 +1,12 @@
+myApp.controler("nhanVienController", function ($http, $scope) {
+    $scope.listNhanVien = [];
+    function fetchNhanVienList() {
+      $http
+        .get("http://localhost:8080/api/nhan-vien/hien-thi")
+        .then(function (response) {
+          $scope.listNhanVien = response.data;
+        });
+    }
+    fetchNhanVienList();
+  });
+  
