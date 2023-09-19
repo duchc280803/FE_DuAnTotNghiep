896e69841f6ee50ecfd8b96e8611e8c5f9d61df7
kjuDyHatC <duchc280803@gmail.com>
2023-09-19 22:30:14 +0700

    update

--- src/controller/nhan-vien-controller.js
+++ src/controller/nhan-vien-controller.js
@@ -1,12 +1,11 @@
-myApp.controler("nhanVienController", function ($http, $scope) {
-    $scope.listNhanVien = [];
-    function fetchNhanVienList() {
-      $http
-        .get("http://localhost:8080/api/nhan-vien/hien-thi")
-        .then(function (response) {
-          $scope.listNhanVien = response.data;
-        });
-    }
-    fetchNhanVienList();
-  });
-  
+myApp.controller("nhanVienController", function ($http, $scope) {
+  $scope.listNhanVien = [];
+  function fetchNhanVienList() {
+    $http
+      .get("http://localhost:8080/api/nhan-vien/hien-thi")
+      .then(function (response) {
+        $scope.listNhanVien = response.data;
+      });
+  }
+  fetchNhanVienList();
+});
