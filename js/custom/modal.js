4fc0e867d9f1a5a181d82d74ff706163c7da508e
kjuDyHatC <duchc280803@gmail.com>
2023-10-03 17:14:33 +0700

    no message

--- js/custom/modal.js
+++ js/custom/modal.js
@@ -1,15 +1,9 @@
-var isModalShown = false;
-    
-    $('#exampleModal').on('shown.bs.modal', function () {
-      isModalShown = true;
-    });
-    
-    $('#exampleModal').on('hidden.bs.modal', function () {
-      isModalShown = false;
-    });
-    
-    function changeModalTarget() {
-      if (isModalShown) {
-        $('#exampleModal1').modal('show');
-      }
-    }
+function showModal() {
+  var modal = document.getElementById("modal");
+  modal.style.display = "flex"; // Hiển thị modal
+}
+
+function hideModal() {
+  var modal = document.getElementById("modal");
+  modal.style.display = "none"; // Ẩn modal
+}
