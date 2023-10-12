17e1f52233c458b7f02e41d064080039671b2764
kjuDyHatC <duchc280803@gmail.com>
2023-10-13 00:46:56 +0700

    update

---
+++ src/customer/routes-customer.js
@@ -0,0 +1,41 @@
+myAppCustom.config(function ($routeProvider, $locationProvider) {
+  $locationProvider.hashPrefix("");
+
+  $routeProvider
+    .when("/home", {
+      templateUrl: "./pages/home.html",
+      controller: "homeController",
+    })
+    .when("/shop", {
+      templateUrl: "./pages/shop-grid.html",
+      controller: "sanPhamShopController",
+    })
+    .when("/shop/detail/:name", {
+      templateUrl: "./pages/shop-details.html",
+      controller: "sanPhamShopDetailController",
+    })
+    .when("/check-out", {
+      templateUrl: "./pages/shoping-cart.html",
+      controller: "CartController",
+    })
+    .when("/thank-you", {
+      templateUrl: "./pages/thankyou.html",
+    })
+    .when("/don-hang", {
+      templateUrl: "./pages/don-hang.html",
+    })
+    .when("/login", {
+      templateUrl: "/src/customer/pages/login.html",
+      controller: "loginCutomController",
+    })
+    .when("/don-hang-chi-tiet", {
+      templateUrl: "./pages/detail-don-hang.html",
+    })
+    .when("/dia-chi", {
+      templateUrl: "./pages/dia-chi.html",
+      controller: "DiaChiController",
+    })
+    .otherwise({
+      redirectTo: "/home",
+    });
+});
