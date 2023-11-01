20b0af5697099c59c30ac609b2011e4affb05fad
DESKTOP-H0QNT1J\ACER <namndph22694@fpt.edu.vn>
2023-11-01 21:20:05 +0700

    check out not login

--- src/controller/shopping-cart-controller.js
+++ src/controller/shopping-cart-controller.js
@@ -1,11 +1,12 @@
 var app = angular.module('myApp', []);
-app.controller('CartController', function ($scope, $http) {
+app.controller('CartController', function ($scope, $http, $window) {
     function loadToTals() {
         // Gọi API và cập nhật giá trị totalAmount
         $http.get("http://localhost:8080/api/gio-hang-chi-tiet-not-login/total-amount?idgh=DB9FDBFD-2BF8-4F9B-A567-8415ED68A02E")
             .then(function (response) {
                 // Lấy giá trị tổng tiền từ phản hồi API
                 $scope.totalAmount = response.data[0].tongSoTien;
+                $window.localStorage.setItem("totalAmount", $scope.totalAmount);
             })
             .catch(function (error) {
                 console.error("Lỗi khi gọi API: " + error);
@@ -36,6 +37,7 @@ app.controller('CartController', function ($scope, $http) {
                 function () {
                     loadCart();
                     loadToTals();
+                    loadNameAndPrice();
                 }
             ]
         });
@@ -92,22 +94,92 @@ app.controller('CartController', function ($scope, $http) {
         $http.get(apiURL)
             .then(function (response) {
                 $scope.products = response.data; // Dữ liệu sản phẩm từ API
+                $window.localStorage.setItem(
+                    "listCart",
+                    $scope.products.map((item) => item.id)
+                );
             });
+
     }
     loadCart();
     // Gọi API và cập nhật danh sách sản phẩm và tổng tiền
-    $http.get("http://localhost:8080/api/gio-hang-chi-tiet-not-login/name-quantity?idgh=DB9FDBFD-2BF8-4F9B-A567-8415ED68A02E")
-        .then(function (response) {
-            $scope.items = response.data;
+    function loadNameAndPrice() {
+        $http.get("http://localhost:8080/api/gio-hang-chi-tiet-not-login/name-quantity?idgh=DB9FDBFD-2BF8-4F9B-A567-8415ED68A02E")
+            .then(function (response) {
+                $scope.items = response.data;
 
-            // Tính tổng tiền sản phẩm
-            $scope.totalAmount = 0;
-            for (var i = 0; i < $scope.items.length; i++) {
-                $scope.totalAmount += parseFloat($scope.items[i].tongTien);
+                // Tính tổng tiền sản phẩm
+                $scope.totalAmount = 0;
+                for (var i = 0; i < $scope.items.length; i++) {
+                    $scope.totalAmount += parseFloat($scope.items[i].tongTien);
+                }
+            })
+            .catch(function (error) {
+                console.error("Lỗi khi gọi API: " + error);
+            });
+    }
+    loadNameAndPrice();
+    // Khai báo biến để lưu các giá trị đã binding
+    $scope.hoTen = "";
+    $scope.soDienThoai = "";
+    $scope.email = "";
+    $scope.diaChi = "";
+    $scope.thanhPho = "";
+    $scope.quanHuyen = "";
+    $scope.phuongXa = "";
+    $scope.tongTien = 0; // Để tránh lỗi nếu không có giá trị tổng tiền
+    $scope.tienKhachTra = 0; // Để tránh lỗi nếu không có giá trị tiền khách trả
+    $scope.gioHangChiTietList = $scope.gioHangChiTietList
+
+    var idGioHangChiTiet = $window.localStorage.getItem("listCart");
+    var gioHangChiTietList = idGioHangChiTiet.split(",");
+    console.log(gioHangChiTietList);
+
+    // Lấy giá trị tổng tiền từ localStorage
+    var totalAmount = parseFloat($window.localStorage.getItem("totalAmount"));
+
+    // Hàm xử lý khi người dùng click nút "Đặt Hàng"
+    $scope.thanhToan = function () {
+        // Hiển thị Sweet Alert để xác nhận
+        Swal.fire({
+            title: "Xác nhận thanh toán?",
+            text: "Bạn có chắc chắn muốn thanh toán đơn hàng?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: "Thanh toán",
+            cancelButtonText: "Hủy",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Nếu người dùng xác nhận thanh toán, tiến hành gửi dữ liệu lên server
+                var data = {
+                    "hoTen": $scope.hoTen,
+                    "soDienThoai": $scope.soDienThoai,
+                    "email": $scope.email,
+                    "diaChi": $scope.diaChi,
+                    "thanhPho": $scope.thanhPho,
+                    "quanHuyen": $scope.quanHuyen,
+                    "phuongXa": $scope.phuongXa,
+                    "tongTien": totalAmount,
+                    "tienKhachTra": $scope.tienKhachTra,
+                    "gioHangChiTietList": gioHangChiTietList
+                };
+
+                // Gửi dữ liệu POST đến server
+                $http({
+                    method: 'POST',
+                    url: 'http://localhost:8080/api/checkout-not-login/thanh-toan',
+                    data: data
+                }).then(function (response) {
+                    // Xử lý phản hồi từ máy chủ nếu cần
+                    $window.location.href = 'thankyou.html';
+                }, function (error) {
+                    // Xử lý lỗi nếu có
+                });
             }
-        })
-        .catch(function (error) {
-            console.error("Lỗi khi gọi API: " + error);
         });
+    };
+
 
 });
