3fa8945c31a91204a3285f416fb82d0535354084
kjuDyHatC <duchc280803@gmail.com>
2023-11-14 11:54:12 +0700

    update

--- src/routes.js
+++ src/routes.js
@@ -76,6 +76,9 @@ myApp.config(function ($routeProvider, $locationProvider) {
     .when("/proudct-new", {
       templateUrl: "./pages/san-pham-new.html",
     })
+    .when("/san-pham-chi-tiet", {
+      templateUrl: "./pages/sanphamchitiet.html",
+    })
     .when("/san-pham", {
       templateUrl: "./pages/san-pham.html",
     })
