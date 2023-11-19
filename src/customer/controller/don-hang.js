55d4fb61f4f826f77f8f64974ce4578ae7f20bcb
DESKTOP-H0QNT1J\ACER <namndph22694@fpt.edu.vn>
2023-11-19 23:28:45 +0700

    cmt nam

--- src/customer/controller/don-hang.js
+++ src/customer/controller/don-hang.js
@@ -1,25 +1,25 @@
-// myApp.controller("donHangController", function ($http, $scope, $window, $routeParams) {
-//     $scope.listDonHang = [];
+myAppCustom.controller("donHangController", function ($http, $scope, $window, $routeParams) {
+    $scope.listDonHang = [];
 
-//     function getDonHang() {
-//         var token = $window.localStorage.getItem("token");
-//         var config = {
-//             headers: {
-//                 Authorization: "Bearer " + token,
-//             },
-//         };
+    function getDonHang() {
+        var token = $window.localStorage.getItem("token");
+        var config = {
+            headers: {
+                Authorization: "Bearer " + token,
+            },
+        };
 
-//         var url = "http://localhost:8080/api/v1/don-hang-khach-hang/hien-thi";
+        var url = "http://localhost:8080/api/v1/don-hang-khach-hang/hien-thi";
 
-//         $http.get(url, config)
-//             .then(function (response) {
-//                 $scope.listDonHang = response.data;
-//             })
-//             .catch(function (error) {
-//                 console.error("Error fetching don hang data:", error);
-//             });
-//     }
+        $http.get(url, config)
+            .then(function (response) {
+                $scope.listDonHang = response.data;
+            })
+            .catch(function (error) {
+                console.error("Error fetching don hang data:", error);
+            });
+    }
 
-//     // Gọi hàm getDonHang khi controller được khởi tạo
-//     getDonHang();
-// });
+    // Gọi hàm getDonHang khi controller được khởi tạo
+    getDonHang();
+});
